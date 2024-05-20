d733767cb14b413c5e339193dea78c226278bd6d
ph.zh <p.zhavoronkov@gmail.com>
2024-05-20 12:58:42 +0200

    tempalte creation

---
+++ src/Praktikum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+        System.out.println("test message");
+
+//        if (isLeapYear(year)) {
+//            // здесь нужно вывести результат
+//        }
+//    }
+//
+//    public static boolean isLeapYear(int year) {
+//        // здесь нужно определить, является ли переданный год високосным
+//    }
+    }
+}
